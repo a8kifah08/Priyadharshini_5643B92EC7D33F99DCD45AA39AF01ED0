c72cd4eb72b2408e2cebc9d993f2ce9b8546cdec
a8kifah08 <144603919+a8kifah08@users.noreply.github.com>
2023-09-25 08:13:57 -0700

    Update main (2.1).py

--- chellange unit-2/main (4).py
+++ chellange unit-2/main (4).py
@@ -1,14 +1,30 @@
-def linear_search_product(product_list, target_product):
-    indices = []
+class BankAccount:
+    def _init_(self, account_number, account_holder_name, initial_balance=0.0):
+        self.__account_number = account_number
+        self.__account_holder_name = account_holder_name
+        self.__account_balance = initial_balance
 
-    for index, product in enumerate(product_list):
-        if product == target_product:
-            indices.append(index)
+    def deposit(self, amount):
+        if amount > 0:
+            self.__account_balance += amount
+            print("Deposited ₹{}. New balance: ₹{}".format(amount, self.__account_balance))
+        else:
+            print("Invalid deposit amount.")
 
-    return indices
+    def withdraw(self, amount):
+        if amount > 0 and amount <= self.__account_balance:
+            self.__account_balance -= amount
+            print("Withdrew ₹{}. New balance: ₹{}".format(amount, self.__account_balance))
+        else:
+            print("Invalid withdrawal amount or insufficient balance.")
+
+    def display_balance(self):
+        print("Account balance for {} (Account #{}): ₹{}".format(
+            self._account_holder_name, self.account_number, self._account_balance))
 
 # Example usage:
-products = ["shoes", "boot", "loafer", "shoes", "sandal", "shoes"]
-target = "shoes"
-result = linear_search_product(products, target)
-print(result)
+account = BankAccount("12345", "John Doe", 1000.0)
+
+account.deposit(500)
+account.withdraw(200)
+account.display_balance()
