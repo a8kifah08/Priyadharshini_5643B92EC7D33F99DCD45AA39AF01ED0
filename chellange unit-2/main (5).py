6da89dfa601f5ad8f4313ad3e2f34e38e27e8dec
a8kifah08 <144603919+a8kifah08@users.noreply.github.com>
2023-09-25 08:14:57 -0700

    Update main (2.2).py

--- chellange unit-2/main (5).py
+++ chellange unit-2/main (5).py
@@ -1,24 +1,22 @@
-class Student:
-    def __init__(self, name, roll_number, cgpa):
-        self.name = name
-        self.roll_number = roll_number
-        self.cgpa = cgpa
+class player:
+  def play(self):
+      print("The player is playing cricket.")
 
-def sort_students(student_list):
-    # Sort the list of students in descending order of CGPA
-    sorted_students = sorted(student_list, key=lambda student: student.cgpa, reverse=True)
-    return sorted_students
 
-# Example usage:
-students = [
-    Student("Stephen", "A123", 7.8),
-    Student("Karthik", "A124", 8.9),
-    Student("Santhosh", "A125", 9.1),
-    Student("Komban", "A126", 9.9),
-]
+class Batsman(player):
+  def play(self):
+        print("The batsman is batting.")
 
-sorted_students = sort_students(students)
 
-# Print the sorted list of students
-for student in sorted_students:
-    print("Name: {}, Roll Number: {}, CGPA: {}".format(student.name, student.roll_number, student.cgpa))
+class Bowler(player):
+  def play(self):
+       print("The bowler is bowling.")
+
+
+batsman = Batsman()
+bowler = Bowler()
+
+
+batsman.play()
+bowler.play()
+   
